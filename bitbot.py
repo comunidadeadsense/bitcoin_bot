0efb701209bef49101cd53516c2f58799a12b692
Jefferson Botelho <contatododriver@gmail.com>
2026-02-08 17:04:25 +0000

    Primeiro commit do projeto 5

---
+++ bitbot.py
@@ -0,0 +1,54 @@
+import ssl
+import json
+
+import websocket
+
+
+def comprar():
+    pass
+
+def vender():
+    pass
+
+
+
+def ao_abrir(ws):
+    print("abriu a Conexão")
+
+    json_subscribe = """
+{
+    "event": "bts:subscribe",
+    "data": {
+        "channel": "live_trades_btcusd"
+    }
+}
+"""
+    ws.send(json_subscribe)
+
+def ao_fechar(ws):
+    print("Fechou a Conexão")
+
+def erro(ws, erro):
+    print("Deu erro")
+    print(erro)
+
+def ao_receber_mensagem(ws, mensagem):
+    mensagem = json.loads(mensagem)
+    price = mensagem['data']['price']
+    print(price)
+
+    if price > 90000:
+        vender()
+    elif price < 65000:
+        comprar()
+    else:
+        print("Aguardar")
+
+
+if __name__ == "__main__":
+    ws = websocket.WebSocketApp("wss://ws.bitstamp.net.",
+                                on_open=ao_abrir,
+                                on_close=ao_fechar,
+                                on_message=ao_receber_mensagem,
+                                on_error=erro)
+    ws.run_forever(sslopt={"cert_reqs": ssl.CERT_NONE})
